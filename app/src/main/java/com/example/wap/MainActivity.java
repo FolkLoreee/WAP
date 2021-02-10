ed8e4710d1c5cd1dae23c3611ffb3b07ed136d82
Sherene <sherene_tan@mymail.sutd.edu.sg>
2021-02-10 14:59:34 +0800

    added wifi scanning

--- app/src/main/java/com/example/wap/MainActivity.java
+++ app/src/main/java/com/example/wap/MainActivity.java
@@ -1,14 +1,125 @@
 package com.example.wap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "WAP";
+    private static final int MY_REQUEST_CODE = 123;
+
+    Button wifiScan;
+    TextView wifiResults;
+
+    WifiManager wifiManager;
+    WifiBroadcastReceiver wifiReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wifiScan = findViewById(R.id.wifiScan);
+        wifiResults = findViewById(R.id.wifiResults);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        // Instantiate broadcast receiver
+        wifiReceiver = new WifiBroadcastReceiver();
+
+        // Register the receiver
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+        wifiScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                askAndStartScanWifi();
+            }
+        });
+
+    }
+
+    private void askAndStartScanWifi()  {
+
+        // With Android Level >= 23, you have to ask the user
+        // for permission to Call.
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) { // 23
+            int permission1 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+            // Check for permissions
+            if (permission1 != PackageManager.PERMISSION_GRANTED) {
+
+                Log.d(LOG_TAG, "Requesting Permissions");
+
+                // Request permissions
+                ActivityCompat.requestPermissions(this,
+                        new String[] {
+                                Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_WIFI_STATE,
+                                Manifest.permission.ACCESS_NETWORK_STATE
+                        }, MY_REQUEST_CODE);
+                return;
+            }
+            Log.d(LOG_TAG, "Permissions Already Granted");
+        }
+        doStartScanWifi();
+    }
+
+    private void doStartScanWifi()  {
+        wifiManager.startScan();
+    }
+
+    @Override
+    protected void onStop()  {
+        this.unregisterReceiver(this.wifiReceiver);
+        super.onStop();
+    }
+
+    // Define class to listen to broadcasts
+    class WifiBroadcastReceiver extends BroadcastReceiver  {
+        @Override
+        public void onReceive(Context context, Intent intent)   {
+            Log.d(LOG_TAG, "onReceive()");
+
+            Toast.makeText(MainActivity.this, "Scan Complete!", Toast.LENGTH_SHORT).show();
+
+            boolean ok = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
+
+            if (ok)  {
+                Log.d(LOG_TAG, "Scan OK");
+
+                List<ScanResult> list = wifiManager.getScanResults();
+
+                for (ScanResult result: list) {
+                    System.out.println(result);
+                }
+
+            }  else {
+                Log.d(LOG_TAG, "Scan not OK");
+            }
+        }
     }
 }
+
