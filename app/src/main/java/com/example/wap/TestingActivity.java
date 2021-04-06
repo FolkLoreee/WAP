8729eed4eab0ba2bc977b23da1123ba9276ddcab
Sherene <sherene_tan@mymail.sutd.edu.sg>
2021-04-06 15:36:05 +0800

    revert the algorithm code

--- app/src/main/java/com/example/wap/TestingActivity.java
+++ app/src/main/java/com/example/wap/TestingActivity.java
@@ -228,7 +228,6 @@ public class TestingActivity extends AppCompatActivity {
                     ArrayList<String> signalsIDs = point.getSignalIDs();
                     pointsFB.put(pointID, signalsIDs);
                     pointsCoordinatesFB.put(pointID, point.getCoordinate());
-                    // Log.d("FIREBASE POINTS", pointID + "(" + point.getCoordinate().getX() + ", " + point.getCoordinate().getY() + ")" + " - " + signalsIDs.toString());
                 }
             }
         });
@@ -320,7 +319,7 @@ public class TestingActivity extends AppCompatActivity {
 
             // continue scanning if it has not reached 4 scans + increase numOfScans
             numOfScans++;
-            if (numOfScans < 4) {
+            if (numOfScans < 2) {
                 wifiManager.startScan();
             }
         }
@@ -347,12 +346,12 @@ public class TestingActivity extends AppCompatActivity {
         preMatching();
 
         //create the Algorithm object
-        Algorithm algorithm = new Algorithm(fingerprintOriginalAvgSignal, fingerprintAvgSignal, fingerprintStdDevSignal, fingerprintCoordinate);
+        // Algorithm algorithm = new Algorithm(fingerprintOriginalAvgSignal, fingerprintAvgSignal, fingerprintStdDevSignal, fingerprintCoordinate);
 
         // weighted fusion
-        Coordinate calculatedPoint1 = algorithm.euclideanDistance(targetData, targetStdDev, targetMacAdd);
-        Coordinate calculatedPoint2 = algorithm.jointProbability(targetDataOriginal, targetMacAdd);
-        Coordinate finalPoint = algorithm.weightedFusion(calculatedPoint1, calculatedPoint2);
+        Coordinate calculatedPoint1 = euclideanDistance();
+        Coordinate calculatedPoint2 = jointProbability();
+        Coordinate finalPoint = weightedFusion(calculatedPoint1, calculatedPoint2);
 
         StringBuilder sb = new StringBuilder();
         sb.append("Euclidean Distance results: x = ");
@@ -463,7 +462,6 @@ public class TestingActivity extends AppCompatActivity {
      * targetDataOriginal = list of original average wifi signal values for each mac address (FOR NOW, THESE HAVE THE SAME VALUES AS THE PROCESSED ONES)
      * Retrieve the corresponding average and standard deviation values for each mac address by the index value
      * */
-    /*
 
     public Coordinate euclideanDistance() {
         double numeratorX = 0;
@@ -652,7 +650,5 @@ public class TestingActivity extends AppCompatActivity {
         }
         return Math.log10(probability);
     }
-
-     */
 
 }
