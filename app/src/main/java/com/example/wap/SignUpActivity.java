cfb6893cf503d1f9cc7760e038b9fa54ade44ecd
Sherene <sherene_tan@mymail.sutd.edu.sg>
2021-04-23 17:36:23 +0800

    fixed signup activity

--- app/src/main/java/com/example/wap/SignUpActivity.java
+++ app/src/main/java/com/example/wap/SignUpActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.wap.firebase.WAPFirebase;
 import com.example.wap.models.Authorization;
 import com.example.wap.models.User;
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -53,76 +55,89 @@ public class SignUpActivity extends AppCompatActivity {
         signupButton = findViewById(R.id.signupButton);
         authSpinner = findViewById(R.id.authSpinner);
 
+
+        User newUser = new User();
         firebaseAuth = FirebaseAuth.getInstance();
         userWAPFirebase = new WAPFirebase<>(User.class,"users");
         ArrayAdapter<Authorization> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Authorization.values());
         authSpinner.setAdapter(arrayAdapter);
+        authSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                                  @Override
+                                                  public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                                                      if(i==0){
+                                                          newUser.setAuth(Authorization.USER);
+                                                      }
+                                                      else{
+                                                          newUser.setAuth(Authorization.ADMIN);
+                                                      }
+                                                  }
 
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                                                  @Override
+                                                  public void onNothingSelected(AdapterView<?> adapterView) {
+                                                      newUser.setAuth(Authorization.USER);
+
+                                                  }
+                                              });
+
+                signupButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
 
 
-                boolean emailValid = AuthUtils.validateEmail(etEmailSignup.getText().toString());
-                //TODO: check if there is any other validations required
-                if (emailValid) {
+                        boolean emailValid = AuthUtils.validateEmail(etEmailSignup.getText().toString());
+                        //TODO: check if there is any other validations required
+                        if (emailValid) {
 
-                    //Create a new User object
-                    User newUser = new User();
-                    newUser.setUsername(etUsernameSignup.getText().toString());
-                    newUser.setEmail(etEmailSignup.getText().toString());
+                            //Create a new User object
+                            newUser.setUsername(etUsernameSignup.getText().toString());
+                            newUser.setEmail(etEmailSignup.getText().toString());
 
-                    //Implement a spinner to choose Authorization
-                    newUser.setAuth(Authorization.ADMIN);
 
-                    //Signing up the user to firebase auth and firebase firestore
-                    firebaseAuth.createUserWithEmailAndPassword(newUser.getEmail(), etPasswordSignup.getText().toString())
+                            //Signing up the user to firebase auth and firebase firestore
+                            firebaseAuth.createUserWithEmailAndPassword(newUser.getEmail(), etPasswordSignup.getText().toString())
 
-                            //? If Firebase Auth creation succeeds
-                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                                @Override
-                                public void onSuccess(AuthResult authResult) {
-                                    //Create Firestore User object
-                                    userWAPFirebase.create(newUser, firebaseAuth.getCurrentUser().getUid()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    //? If Firebase Auth creation succeeds
+                                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                                         @Override
-                                        public void onSuccess(Void aVoid) {
-                                            Log.d(TAG, "User " + newUser.getUsername() + " has been successfully created");
-                                            Toast.makeText(SignUpActivity.this, "Sign up successful!", Toast.LENGTH_SHORT).show();
+                                        public void onSuccess(AuthResult authResult) {
+                                            //Create Firestore User object
+                                            userWAPFirebase.create(newUser, firebaseAuth.getCurrentUser().getUid()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Log.d(TAG, "User " + newUser.getUsername() + " has been successfully created");
+                                                    Toast.makeText(SignUpActivity.this, "Sign up successful!", Toast.LENGTH_SHORT).show();
 
-                                            //Go back to Login Activity
-                                            Intent loginIntent = new Intent(SignUpActivity.this, LoginActivity.class);
-                                            loginIntent.putExtra(PASSWORD_KEY, etPasswordSignup.getText().toString());
-                                            loginIntent.putExtra(EMAIL_KEY, newUser.getEmail());
-                                            startActivity(loginIntent);
+                                                    //Go back to Login Activity
+                                                    Intent loginIntent = new Intent(SignUpActivity.this, LoginActivity.class);
+                                                    loginIntent.putExtra(PASSWORD_KEY, etPasswordSignup.getText().toString());
+                                                    loginIntent.putExtra(EMAIL_KEY, newUser.getEmail());
+                                                    startActivity(loginIntent);
+                                                }
+                                            });
+                                        }
+                                    })
+                                    //! If Firebase Auth User creation fails
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            if (e instanceof FirebaseAuthUserCollisionException) {
+                                                if (((FirebaseAuthUserCollisionException) e).getErrorCode().equals("ERROR_EMAIL_ALREADY_IN_USE")) {
+                                                    Log.w(TAG, "Email is already in use.");
+                                                    Toast.makeText(SignUpActivity.this, "Email is already in use", Toast.LENGTH_SHORT).show();
+                                                }
+                                            } else if (e instanceof FirebaseAuthWeakPasswordException) {
+                                                Log.w(TAG, "Weak password detected.");
+                                                Toast.makeText(SignUpActivity.this, "Password must be at least 6 characters long", Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                Log.e(TAG, "Error in creating Firebase Auth user: " + e.toString(), e);
+                                                Toast.makeText(SignUpActivity.this, "Sign up failed", Toast.LENGTH_SHORT).show();
+                                            }
                                         }
                                     });
-                                }
-                            })
-                            //! If Firebase Auth User creation fails
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    if (e instanceof FirebaseAuthUserCollisionException) {
-                                        if (((FirebaseAuthUserCollisionException) e).getErrorCode().equals("ERROR_EMAIL_ALREADY_IN_USE")) {
-                                            Log.w(TAG, "Email is already in use.");
-                                            Toast.makeText(SignUpActivity.this, "Email is already in use", Toast.LENGTH_SHORT).show();
-                                        }
-                                    }
-                                    else if(e instanceof FirebaseAuthWeakPasswordException){
-                                        Log.w(TAG, "Weak password detected.");
-                                        Toast.makeText(SignUpActivity.this, "Password must be at least 6 characters long", Toast.LENGTH_SHORT).show();
-                                    }
-                                    else {
-                                        Log.e(TAG, "Error in creating Firebase Auth user: "+e.toString(),e);
-                                        Toast.makeText(SignUpActivity.this, "Sign up failed", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
-                }
-                else{
-                    Toast.makeText(SignUpActivity.this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
